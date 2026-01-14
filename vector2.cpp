c698c19e25b8119075b754cad063992a98278819
Fernando <ferbrizie@gmail.com>
2026-01-14 16:35:04 +0100

    check the rest of operators ands retry

--- vector2.cpp
+++ vector2.cpp
@@ -2,6 +2,8 @@
 
 vector2::vector2()
 {
+	this->x = 0;
+	this->y = 0;
 	std::cout<<"vector2 standard constructor called"<<std::endl;
 }
 
@@ -12,39 +14,93 @@ vector2::vector2(int num1, int num2)
 	std::cout<<"Parametirized constructor called"<<std::endl;
 }
 
-vector2::~vector2
+vector2::vector2(const vector2& other)
+{
+	*this = other;
+}
+
+vector2::~vector2()
 {
 	std::cout<<"vector2 destructor called"<<std::endl;
 }
 
-vector2& vector2::operator=(vector2 &other)
+vector2& vector2::operator=(const vector2 &other)
 {
 	if(this == &other)
-		return *this
-	this->x = source.x
-	this->y = sosurce.y
+		return *this;
+	this->x = other.x;
+	this->y = other.y;
 	return(*this);
 }
 
-vector2 vector2::operator[](int i) const
+int& vector2::operator[](int i)
 {
 	if(i == 0)
 		return (this->x);
 	return (this->y);
 }
 
+int vector2::operator[](int i) const
+{
+	if(i == 0)
+		return (this->x);
+	return (this->y);
+}
+
+//+ & - operators
 vector2 vector2::operator-() const
 {
 	vector2 temp = *this;
-	temp[0] =this[0];
-	temp[1] = this[1];
+	temp[0] = -temp[0];
+	temp[1] = -temp[1];
 	return (temp);
 }
 
-	temp[0] =this[0];
-	temp[1] = this[1];
+//++ pre and post
+vector2 &vector2::operator++()
+{
+	this->x += 1;
+	this->y += 1;
+	return (*this);
+}
+vector2 vector2::operator++(int)
+{
+	vector2 temp = *this;
+	this->x += 1;
+	this->y += 1;
 	return (temp);
-std::ostream& vector2::operator<<(std::ostream &ostream, const vect2 &v) const
+}
+// -- pre and post
+vector2 &vector2::operator--()
+{
+	this->x -= 1;
+	this->y -= 1;
+	return (*this);
+}
+vector2 vector2::operator--(int)
+{
+	vector2 temp = *this;
+	this->x += 1;
+	this->y += 1;
+	return (temp);
+}
+//+= & -=
+
+vector2 &vector2::operator+=(const vector2& other)
+{
+	this->x += other.x;
+	this->y += other.y;
+	return (*this);
+}
+vector2 &vector2::operator-=(const vector2& other)
+{
+	this->x -= other.x;
+	this->y -= other.y;
+	return (*this);
+}
+
+
+std::ostream& operator<<(std::ostream &ostream, const vector2 &v)
 {
 	std::cout<< "{" << v[0] << ", " << v[1] << "}"<<std::endl;
 	return (ostream);
--- vector2.hpp
+++ vector2.hpp
@@ -12,5 +12,19 @@ class vector2
 	public:
 		vector2();
 		vector2(int x, int y);
+		vector2(const vector2 &other);
 		~vector2();
-}
+		vector2& operator=(const vector2 &other);
+		int& operator[](int i) ;
+		int operator[](int i) const;
+		vector2 operator-() const;
+		vector2 operator+() const;
+		vector2 &operator++();
+		vector2 operator++(int);
+		vector2 &operator--();
+		vector2 operator--(int);
+		vector2 &operator+=(const vector2& other);
+		vector2 &operator-=(const vector2& other);
+};
+		std::ostream& operator<<(std::ostream &ostream, const vector2 &v);
+#endif
